a7eae8c91cace00e2758e8384a9c3d49549170f8
Brandon Edwards <bsedwards@ucdavis.edu>
2025-10-16 16:15:24 -0700

    Adding database table

--- CAHFS Recharges/Data/FinancialContext.cs
+++ CAHFS Recharges/Data/FinancialContext.cs
@@ -5,6 +5,8 @@ namespace CAHFS_Recharges.Data
 {
     public class FinancialContext : DbContext
     {
+        public virtual DbSet<FeedBatch> FeedBatches { get; set; }
+
         public FinancialContext()
         {
 
@@ -22,14 +24,53 @@ namespace CAHFS_Recharges.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<ExampleTable>(entity =>
+            modelBuilder.Entity<FeedBatch>(entity =>
             {
-                entity.HasKey(e => e.Id);
-                entity.ToTable("TableName");
+                entity.ToTable("C_AE_Feed_Batch", "dbo");
 
-                entity.Property(e => e.Column)
-                    .HasMaxLength(1000)
-                    .HasColumnName("my_column");
+                entity.HasKey(e => e.BatchID)
+                    .HasName("PK_C_AE_FEED_HEADER");
+                entity.Property(e => e.BatchID)
+                    .HasColumnName("batchID")
+                    .IsRequired();
+                entity.Property(e => e.DateSent)
+                    .HasColumnName("dateSent")
+                    .HasColumnType("datetime");
+                entity.Property(e => e.PickupSentFlag)
+                    .HasColumnName("pickupSentFlag")
+                    .HasColumnType("char(1)")
+                    .IsRequired();
+                entity.Property(e => e.AEConsumerReferenceID)
+                    .HasColumnName("AEConsumerReferenceID")
+                    .HasMaxLength(80)
+                    .IsRequired();
+                entity.Property(e => e.AEConsumnerNotes)
+                    .HasColumnName("AEConsumnerNotes")
+                    .HasMaxLength(240)
+                    .IsRequired();
+                entity.Property(e => e.AERequestStatus)
+                    .HasColumnName("AERequestStatus")
+                    .HasMaxLength(10);
+                entity.Property(e => e.ErrorDetail)
+                    .HasColumnName("ErrorDetail")
+                    .HasMaxLength(500);
+                entity.Property(e => e.AEConsumerRequestID)
+                    .HasColumnName("AEConsumerRequestID");
+                entity.Property(e => e.AETransactionDate)
+                    .HasColumnName("AETransactionDate")
+                    .HasColumnType("datetime");
+                entity.Property(e => e.AEJournalName)
+                    .HasColumnName("AEJournalName")
+                    .HasMaxLength(100);
+                entity.Property(e => e.AEJournalDescription)
+                    .HasColumnName("AEJournalDescription")
+                    .HasMaxLength(240);
+                entity.Property(e => e.AEJournalReference)
+                    .HasColumnName("AEJournalReference")
+                    .HasMaxLength(25);
+                entity.Property(e => e.BatchTotal)
+                    .HasColumnName("batchTotal")
+                    .HasColumnType("numeric(19, 2)");
             });
         }
     }
--- CAHFS Recharges/Pages/Index.cshtml.cs
+++ CAHFS Recharges/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using CAHFS_Recharges.Data;
 using CAHFS_Recharges.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,15 +9,28 @@ namespace CAHFS_Recharges.Pages
     public class IndexModel : PageModel
     {
         private readonly Logger _logger;
+        private readonly FinancialContext _context;
 
-        public IndexModel()
+        public IndexModel(FinancialContext context)
         {
+            _context = context;
             _logger = LogManager.GetCurrentClassLogger();
         }
 
         public void OnGet()
         {
             var user = HttpHelper.HttpContext?.User?.Identity?.Name;
+            bool canAccessDatabase = false;
+            try
+            {
+                var feedbatches = _context.FeedBatches.Take(10).ToList();
+                canAccessDatabase = true;
+            }
+            catch (Exception ex)
+            {
+            }
+
+            ViewData["CanAccessDatabase"] = canAccessDatabase;
             ViewData["User"] = user;
         }
     }
